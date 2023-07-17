027b60b9a6dcbe680d3e99252e4aac91b5d5cd7c
rkhosroshahli <rasa.khosrowshahli@ontariotechu.net>
2023-07-17 15:36:04 -0400

    package json update 4

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use(express.static(path.resolve(__dirname, '../frontend/build')));
   app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
   });
 } else {
   app.get("/", (req, res)=>{
     // console.log("jnds")
     res.send("API is running...");
-    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    // res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    // res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
   })
 }
 
